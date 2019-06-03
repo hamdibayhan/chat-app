c967cf1504b6012f312e6853f8ee3b21d49c2dae
Hamdi Bayhan <hamdibayhan@gmail.com>
2019-06-03 23:22:18 +0300

    Bcryptjs and jsonwebtoken were added, /register end point was opened

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+global.__root   = __dirname + '/'; 
+
+var AuthController = require(__root + 'auth/AuthController');
+app.use('/api/auth', AuthController);
 
 module.exports = app;
---
+++ auth/AuthController.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+var User = require('../user/User');
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+
+router.post('/register', function(req, res) {
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    }, 
+    function (err, user) {
+      if (err) return res.status(500).send("There was a problem registering the user.");
+  
+      var token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400
+      });
+  
+      res.status(200).send({ auth: true, token: token });
+    });
+  });
